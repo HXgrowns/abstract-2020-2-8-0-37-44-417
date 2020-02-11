0595942222e18c6110f91e1ab5f5e86c2a9912e3
huxiao <804156017@qq.com>
2020-02-11 21:13:35 +0800

    [huxiao] {refact}: refact abstract code

--- abstract/src/main/java/com/Demo.java
+++ abstract/src/main/java/com/Demo.java
@@ -6,14 +6,6 @@ import java.util.List;
 public class Demo {
 
   public static void main(String[] args) {
-//    Cock cock = new Cock();
-//    Hen hen = new Hen();
-//    ChildChicken childChicken = new ChildChicken();
-//
-//    System.out.format("100元可以买%s %d只\n", cock.getType(), (int)(100 / cock.getPrice()));
-//    System.out.format("100元可以买%s %d只\n", hen.getType(), (int)(100 / hen.getPrice()));
-//    System.out.format("100元可以买%s %d只\n", childChicken.getType(), (int)(100 / childChicken.getPrice()));
-
     List<Chicken> chickens = new ArrayList<>();
     chickens.add(new Cock());
     chickens.add(new Hen());
